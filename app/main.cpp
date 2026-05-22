bc326f5145380df86dc3307ede3fa69d190a13ea
Bl8nk <mo.schmidt1007@gmail.com>
2026-05-22 21:47:39 +0200

    added main

---
+++ app/main.cpp
@@ -0,0 +1,25 @@
+
+#include "Debugger/IDebugger.hpp"
+
+using namespace LinuxDebugger;
+
+int main(int argc, char* argv[]){
+    if (argc < 2) {
+        std::cerr << "Program name not specified.";
+        return -1;
+    }
+
+    auto prog = argv[1];
+    auto pid = fork();
+    if (pid == 0) {
+        //execute debugee
+        ptrace(PTRACE_TRACEME, 0, nullptr, nullptr);
+        execl(prog, prog, nullptr);
+    }
+    else if(pid >= 1){
+        // parent process
+        std::cout << "Started debugging Process. " << pid << std::endl;
+        Debugger dbg{prog, pid};
+        dbg.run();
+    }
+}
